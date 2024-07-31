856041bf2d0f609f2bda70685ccdcef7012ad7f2
lucas-lyra <121949176+lucas-lyra@users.noreply.github.com>
2024-07-31 16:38:36 -0300

    Update paramPadrão.js

--- funçao-js/paramPadrão.js
+++ funçao-js/paramPadrão.js
@@ -19,4 +19,10 @@ function soma2 (a,b,c){
 
 console.log(soma2(),soma2(3),soma2(1,2,3), soma2(0,0,0));
 
+// valor padrao do es 2015
+function soma3 (a = 1 , b = 1, c = 1 ){
+return a + b + c
+}
+console.log(soma3(),soma3(3), soma3(1,2,3),soma3(0,0,0) );
+
 
