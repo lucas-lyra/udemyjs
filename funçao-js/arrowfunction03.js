991c6b29228c56a2bcf547f2af7ef38935803521
lucas-lyra <121949176+lucas-lyra@users.noreply.github.com>
2024-08-08 16:03:42 -0300

    Update arrowfunction03.js

--- funçao-js/arrowfunction03.js
+++ funçao-js/arrowfunction03.js
@@ -3,3 +3,17 @@ let compComThis = function (param){
     
 }
 compComThis(global)
+
+const obj = {} 
+compComThis = compComThis.bind(obj)
+compComThis(global)
+compComThis(obj)
+
+
+let compComThisArrow = param => console.log(this === param);
+compComThisArrow(global)
+compComThisArrow(module.exports)
+
+comparacomthisarrow = compComThisArrow.bind(obj)
+comparacomthisarrow (obj)
+comparacomthisarrow(module.exports)
