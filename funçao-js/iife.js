aa581e2357430d0bfcfd21ec8239715eae06173a
lucas-lyra <121949176+lucas-lyra@users.noreply.github.com>
2024-08-16 15:16:31 -0300

    estudo js

---
+++ funçao-js/iife.js
@@ -0,0 +1,5 @@
+//IIFE -> immedately invoked function expression
+(function(){
+    console.log('sera executado na hora!');
+    console.log('foge do escopo mais abrangente!');}
+)()
---
+++ funçaojs-exercicios/calculoex01.js
@@ -0,0 +1,6 @@
+function calculo(n1,n2){
+    return `soma ${n1+n2}  subtração ${n1 - n2}  multiplicação ${n1 * n2}  divisão ${n1 / n2}`
+} 
+
+
+console.log(calculo(1,2));
