877f14680722c83348f0c037b21fddd6390b2d29
lucas-lyra <121949176+lucas-lyra@users.noreply.github.com>
2024-08-21 12:30:49 -0300

    Update basquete.js

--- funçaojs-exercicios/basquete.js
+++ funçaojs-exercicios/basquete.js
@@ -1,22 +1,23 @@
 let jogos = "30,50,20,3,6,8,10,90,23,14"
 
+function avpuntuacao(jogos) {
+    let pontuacoes = jogos.split(","); 
+    let record = 0;
+    let jogpior = 1;
+    let pontmaior = parseInt(pontuacoes[0]);
+    let pontmenor = parseInt(pontuacoes[0]);
 
-function avpuntuaçao (jogos){
-    let pontuacoes = jogos.split(", ")
-    let record = 0
-    let jogpior = 1
-    let pontmaior = pontuacoes[0]
-    let pontmenor = pontuacoes[0]
-
-    for (let i = 1; i < pontuacoes.length;i++){
-        if(parent(pontuacoes[i])> parseInt(pontmaior)){
-            pontmaior = pontuacoes[i]
-            record++
-        }else if (parseInt(pontuacoes[i])< parseInt(pontmenor)){
-            pontmenor = pontuacoes[i]
-            jogpior = i+1;
+    for (let i = 1; i < pontuacoes.length; i++) {
+        let pontuacaoAtual = parseInt(pontuacoes[i]);
+        if (pontuacaoAtual > pontmaior) {
+            pontmaior = pontuacaoAtual;
+            record++;
+        } else if (pontuacaoAtual < pontmenor) {
+            pontmenor = pontuacaoAtual;
+            jogpior = i + 1;
         }
     }
-    return[record,jogpior]
+    return [record, jogpior];
 }
-console.log(avpuntuaçao(jogos));
+
+console.log(avpuntuacao(jogos));
